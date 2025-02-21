2f6c0de18a6ce13f7e0e679957ae8629eccd8e31
Sean Rokah <seanrokah@gmail.com>
2025-02-21 17:27:09 +0200

    version 1.1.0

--- command_generator.py
+++ command_generator.py
@@ -3,8 +3,6 @@ import os
 import subprocess
 client = OpenAI()
 
-#asking user for a linux command
-
 print ("""
  ██████╗  ██████╗ ███╗   ███╗ █████╗ ███╗   ██╗██████╗    
  ██╔════╝ ██╔═══██╗████╗ ████║██╔══██╗████╗  ██║██╔══██╗    
@@ -19,22 +17,23 @@ Select your operation system(Input the number):
  1. Linux\MacOS
  2. Windows
        """)
-Operation_System = input("Enter the number: ")
 
+Operation_System = input("Enter the number: ")
 if Operation_System == "1":
     Operation_System = "Linux"
 elif Operation_System == "2":
     Operation_System = "Windows"
 else:
     print("Invalid input, please enter 1 for Linux or 2 for Windows")
-
-
-
+    exit()
 command = input("Please describe the command you want: ")
-
-
-#asking gpt to generate a linux command
-
+command_amount = int(input("How many commands do you want to generate? (1-5): "))
+if int(command_amount) > 5:
+    print("Invalid input, please enter a number between 1 and 5")
+    exit()
+elif int(command_amount) < 1:
+    print("Invalid input, please enter a number between 1 and 5")
+    exit()
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
@@ -43,7 +42,8 @@ completion = client.chat.completions.create(
          
          From now on your a {Operation_System} command generator,
           do not answer with any more data but the command (please dont add the bash\powershell\cmd in the begginning)
-        
+          the amount of commands you want to generate is {command_amount}
+          please generate the command in each line without adding any extra data or spaces between the commands
           """},
         {"role": "user", "content": command},
     ]
@@ -52,13 +52,25 @@ completion = client.chat.completions.create(
 
 answer = completion.choices[0].message.content
 
-#run cmd command
-print (answer)
+commands = answer.split('\n')
+print (f"Here are the commands that I have generated for you: {commands}")
+
 
 user_approval = input("Do you want to run the command? (yes/no): ")
-if user_approval == "yes":
-    os.system(answer)
-elif user_approval == "no":
-    print("Ok, I will not run the command")
-else:
-    subprocess.run(["echo", "Invalid input, please enter yes or no"])
+
+def Run_Command():
+    if user_approval == "yes":
+        os.system(commands[i])
+    elif user_approval == "no":
+        print("Ok, I will not run the command")
+    else:
+        print("Invalid input, please enter yes or no")
+        exit()
+
+i = 0
+while i < command_amount:
+    Run_Command()
+    i += 1
+    print(f"Command {i} has been executed")
+
+    
